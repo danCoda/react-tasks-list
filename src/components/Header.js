71fa0e675b21b428d99c1b803f635995d32eea4c
Daniel Son <dson.email@gmail.com>
2021-08-04 16:49:17 +1000

    Routing done. react-router-dom added, and conditional show based on route.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,20 @@
+import { useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types'
 import Button from './Button'
 
 const Header = (props) => {
+    const location = useLocation();
+
     return (
         <header className='header'>
             <h1 style={headingStyle}>{props.title}</h1>
-            <Button 
-                color={props.showAdd ? 'red' : 'green'} 
-                text={props.showAdd ? 'Close' : 'Add'} 
-                onClick={props.onAdd}/>
+            { location.pathname === '/' &&
+                <Button 
+                    color={props.showAdd ? 'red' : 'green'} 
+                    text={props.showAdd ? 'Close' : 'Add'} 
+                    onClick={props.onAdd}
+                />
+            }
         </header>
     )
 }
