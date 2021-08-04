e178bded13a5b223aca91c8ab92f9c72192d8f32
Daniel Son <dson.email@gmail.com>
2021-08-04 15:22:30 +1000

    Can add new tasks.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+import { useState } from 'react';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
-import { useState } from 'react';
+import AddTask from './components/AddTask';
 
 const App = () => {
 
@@ -21,21 +22,38 @@ const App = () => {
         id:3,
         text:"Sleep",
         day:"Saturday 18th June",
-        reminder: true
+        reminder: false
     }
   ]);
+
+  // Add task.
+  const addTask = task => {
+    const id = Math.floor(Math.random() * 1000) + 1;
+    const newTask = { id, ...task };
+    setTasks([ ...tasks, newTask ]);
+  }
 
   // Delete task.
   const deleteTask = id => {
     setTasks(tasks.filter(task => task.id !== id))
   }
 
+  // Toggle reminder. 
+  const toggleReminder = id => {
+    setTasks(tasks.map(task => task.id === id ? {...task, reminder: !task.reminder} : task))
+  }
+
   return (
     <div className="container">
       <Header title='hello'/>
+      <AddTask onAdd={addTask} />
       { tasks.length > 0 ? 
         (
-          <Tasks tasks={tasks} onDelete={deleteTask} /> 
+          <Tasks 
+            tasks={tasks} 
+            onDelete={deleteTask} 
+            onToggle={toggleReminder}
+          /> 
         )
         : 
         ('No tasks to show...') 
