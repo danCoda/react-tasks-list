7aba1d1063842180e77083ef6f094f58f8568a79
Daniel Son <dson.email@gmail.com>
2021-08-04 14:26:28 +1000

    Component added: Tasks. Actual tasks are added 'globally' from App.js.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import Header from './components/Header';
+import Tasks from './components/Tasks';
+import { useState } from 'react';
 
 const App = () => {
+
+  const [tasks, setTasks] = useState([
+    {
+        id:1,
+        text:"Eat food",
+        day:"Monday 12th June",
+        reminder: true
+    },
+    {
+        id:2,
+        text:"Shower",
+        day:"Wednesday 15th June",
+        reminder: true
+    },
+    {
+        id:3,
+        text:"Sleep",
+        day:"Saturday 18th June",
+        reminder: true
+    }
+  ]);
 
   return (
     <div className="container">
       <Header title='hello'/>
+      <Tasks tasks={tasks} />
     </div>
   );
 }
